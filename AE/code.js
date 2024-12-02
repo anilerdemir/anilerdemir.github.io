a3eaa0339d89909df38146afdd940c67a3cb8637
Anıl ERDEMİR <56921458+anilerdemir@users.noreply.github.com>
2024-12-03 00:14:38 +0300

    Update code.js

--- AE/code.js
+++ AE/code.js
@@ -1,6 +1,8 @@
-const AECODE = document.createElement("script");
-AECODE.async = true;
-AECODE.type = "module";
-AECODE.defer = true;
-AECODE.src = "https://anilerdemir.github.io/assets/core/aecode.js";
-document.body.append(AECODE);
+document.addEventListener("DOMContentLoaded", function () {
+    const AE_CODE = document.createElement("script");
+      AE_CODE.async = true;
+      AE_CODE.type = "module";
+      AE_CODE.defer = true;
+      AE_CODE.src = "https://anilerdemir.github.io/assets/core/aecode.js";
+    document.body.append(AE_CODE);
+});
