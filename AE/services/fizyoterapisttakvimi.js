e7d5e67f76377a6c801e948064b7f8a08b21f164
Anıl ERDEMİR <56921458+anilerdemir@users.noreply.github.com>
2024-12-02 22:46:24 +0300

    Update fizyoterapisttakvimi.js

--- AE/services/fizyoterapisttakvimi.js
+++ AE/services/fizyoterapisttakvimi.js
@@ -6,3 +6,17 @@ AECODE.Core.insert(
     AECODE.Core.create(socialMedias)
   );
 AECODE.Module.socialMedia(socialMedias);
+
+AECODE.Core.update(
+  `${AECODE.anilerdemir[1]} style`,
+  {},
+  `${socialMedias}{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    font-size: xx-large;
+  }
+  ${socialMedias} *{
+    margin: 0 5px;
+  });
