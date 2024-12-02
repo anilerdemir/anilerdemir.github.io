20482d8cbadc79bd101df64e181a4785efe7e968
Anıl ERDEMİR <56921458+anilerdemir@users.noreply.github.com>
2024-12-02 22:28:01 +0300

    Update fizyoterapisttakvimi.js

--- AE/services/fizyoterapisttakvimi.js
+++ AE/services/fizyoterapisttakvimi.js
@@ -2,7 +2,7 @@ import * as AECODE from "https://anilerdemir.github.io/assets/core/aecode.js";
 
 const socialMedias = AECODE.Module.randomTag();
 AECODE.Core.insert(
-    ".footerTopTutucu",
+    "#eluidd54dd61b",
     AECODE.Core.create(socialMedias)
   );
 AECODE.Module.socialMedia(socialMedias);
