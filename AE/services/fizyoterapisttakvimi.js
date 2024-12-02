538189d50f7c49963c83dc576f7934818fd766be
Anıl ERDEMİR <56921458+anilerdemir@users.noreply.github.com>
2024-12-03 00:08:44 +0300

    Update fizyoterapisttakvimi.js

--- AE/services/fizyoterapisttakvimi.js
+++ AE/services/fizyoterapisttakvimi.js
@@ -1,9 +1,9 @@
 import * as AECODE from "https://anilerdemir.github.io/assets/core/aecode.js";
 
-const socialmedias = AECODE.Module.randomTag();
-AECODE.message(socialmedias);
-AECODE.Core.insert(".zn_text_box .eluid872b040d .hidden-zn_dummy_value .hidden-zn_dummy_value .hidden-zn_dummy_value .hidden-zn_dummy_value  .zn_text_box-light .element-scheme--light", AECODE.Core.create(socialmedias));
-AECODE.Module.socialMedia(socialmedias);
+const socialMedias = AECODE.Module.randomTag();
+AECODE.message(socialMedias);
+AECODE.Core.insert(".zn_text_box .eluid872b040d .hidden-zn_dummy_value .hidden-zn_dummy_value .hidden-zn_dummy_value .hidden-zn_dummy_value  .zn_text_box-light .element-scheme--light", AECODE.Core.create(socialMedias));
+AECODE.Module.socialMedia(socialMedias);
 
 AECODE.Core.update(
   `${AECODE.anilerdemir[1]} style`,
