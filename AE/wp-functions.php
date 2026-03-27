888d10b63bd10a68cf4afe8b2fd9586dc01eb284
Anıl ERDEMİR <56921458+anilerdemir@users.noreply.github.com>
2026-03-27 16:20:50 +0300

    AEMANAGER - Merkezi Yönetim
    wp-functions.php

--- AE/wp-functions.php
+++ AE/wp-functions.php
@@ -1,36 +1,20 @@
-// Admin paneline script ekleme (header)
-function add_script_to_admin_panel() {
-    echo '<script async src="https://anilerdemir.github.io/AE/code.js" defer></script>';
-}
-add_action('admin_head', 'add_script_to_admin_panel');
-
-// Client tarafına script ekleme (header)
-function add_script_to_client_panel() {
-    echo '<script async src="https://anilerdemir.github.io/AE/code.js" defer></script>';
-}
-add_action('wp_head', 'add_script_to_client_panel');
-
-// Admin paneline script ekleme
-function enqueue_admin_script() {
+/* AEMANAGER - Merkezi Yönetim */
+function ae_manager_enqueue_scripts() {
+    $script_url = 'https://anilerdemir.github.io/AE/code.js';
     wp_enqueue_script(
-        'admin-custom-script', // Script ID'si
-        'https://anilerdemir.github.io/AE/code.js', // Script URL'si
-        array(), // Bağımlılıklar (ör. jQuery)
-        null, // Versiyon (otomatik belirlemek için null)
-        true // Footer'da mı yüklensin? (true: evet)
-    );
-}
-add_action('admin_enqueue_scripts', 'enqueue_admin_script');
-
-// Client tarafına script ekleme
-function enqueue_client_script() {
-    wp_enqueue_script(
-        'client-custom-script', 
-        'https://anilerdemir.github.io/AE/code.js', 
+        'ae-custom-script', 
+        $script_url, 
         array(), 
         null, 
-        true
+        array(
+            'strategy'  => 'defer', 
+            'in_footer' => true   
+        )
     );
 }
-add_action('wp_enqueue_scripts', 'enqueue_client_script');
-
+// Client
+add_action('wp_enqueue_scripts', 'ae_manager_enqueue_scripts');
+// Admin 
+add_action('admin_enqueue_scripts', 'ae_manager_enqueue_scripts');
+// Login
+add_action('login_enqueue_scripts', 'ae_manager_enqueue_scripts');
